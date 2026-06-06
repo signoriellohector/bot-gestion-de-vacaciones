200dfe13104fa96f0620cb03c6f83d07280067ca
Signoriello Hector <hector.signoriello@gmail.com>
2026-06-06 03:53:27 -0300

    Eliminacion del bot antiguo, Creacion de script main.py

--- create_db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from models import Base
-
-# Crear DB SQLite (puedes ajustar a PostgreSQL, MySQL, etc)
-engine = create_engine('sqlite:///vacaciones.db')
-Base.metadata.create_all(engine)
-print("DB y tablas creadas con el modelo original.")
